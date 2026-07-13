70776d800114eb50d58a630b80eb01ea6ad66654
Ning Wang <unidentifiedme26@gmail.com>
2026-07-13 14:47:58 +0800

    chore: add merge-conflict and whitespace pre-commit hooks

--- git-hooks.nix
+++ git-hooks.nix
@@ -6,6 +6,15 @@
     packageOverrides.treefmt = treefmtWrapper;
   };
 
+  # File hygiene for everything treefmt doesn't format (Markdown, config, ...).
+  check-merge-conflicts.enable = true;
+  trim-trailing-whitespace.enable = true;
+  end-of-file-fixer.enable = true;
+  mixed-line-endings = {
+    enable = true;
+    args = [ "--fix=lf" ];
+  };
+
   # This project handles GitHub tokens and managed API tokens; a credential
   # committed by accident is the worst-case bug. Keep this on from day one.
   detect-private-keys.enable = true;
